993dec5d96e883e44521ae2ddecaeb0d85183bae
James Little <alphafoobar@users.noreply.github.com>
2019-05-22 00:09:13 +1200

    Implemented binary search
    Implementation very similar to Java style.

--- src/main/groovy/kata02/BinarySearch.groovy
+++ src/main/groovy/kata02/BinarySearch.groovy
@@ -1,10 +1,23 @@
 package kata02
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException
-
 class BinarySearch {
 
-  int chop(int key, int[] a) {
-    throw new NotImplementedException()
+  int chop(int key, int[] array) {
+    return chopWorker(key, array, 0, array.length)
+  }
+
+  private int chopWorker(int key, int[] array, int offset, int length) {
+    int remaining = length - offset
+    if (remaining == 0) {
+      return -1
+    }
+    int point = (int) (offset + remaining / 2)
+    if (array[point] == key) {
+      return point
+    }
+    if (array[point] > key) {
+      return chopWorker(key, array, offset, point)
+    }
+    return chopWorker(key, array, point + 1, length)
   }
 }
--- src/test/groovy/kata02/BinarySearchTest.groovy
+++ src/test/groovy/kata02/BinarySearchTest.groovy
@@ -17,25 +17,25 @@ class BinarySearchTest extends GroovyTestCase {
 
   void test_three() {
     int[] ints = [1, 3, 5]
-    assert 0, search.chop(1, ints)
-    assert 1, search.chop(3, ints)
-    assert 2, search.chop(5, ints)
-    assert -1, search.chop(0, ints)
-    assert -1, search.chop(2, ints)
-    assert -1, search.chop(4, ints)
-    assert -1, search.chop(6, ints)
+    assert 0 == search.chop(1, ints)
+    assert 1 == search.chop(3, ints)
+    assert 2 == search.chop(5, ints)
+    assert -1 == search.chop(0, ints)
+    assert -1 == search.chop(2, ints)
+    assert -1 == search.chop(4, ints)
+    assert -1 == search.chop(6, ints)
   }
 
   void test_four() {
     int[] ints = [1, 3, 5, 7]
-    assert 0, search.chop(1, ints)
-    assert 1, search.chop(3, ints)
-    assert 2, search.chop(5, ints)
-    assert 3, search.chop(7, ints)
-    assert -1, search.chop(0, ints)
-    assert -1, search.chop(2, ints)
-    assert -1, search.chop(4, ints)
-    assert -1, search.chop(6, ints)
-    assert -1, search.chop(8, ints)
+    assert 0 == search.chop(1, ints)
+    assert 1 == search.chop(3, ints)
+    assert 2 == search.chop(5, ints)
+    assert 3 == search.chop(7, ints)
+    assert -1 == search.chop(0, ints)
+    assert -1 == search.chop(2, ints)
+    assert -1 == search.chop(4, ints)
+    assert -1 == search.chop(6, ints)
+    assert -1 == search.chop(8, ints)
   }
 }
